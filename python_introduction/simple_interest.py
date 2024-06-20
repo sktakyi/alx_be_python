c96278294bedbc8f87f64b88ebf818b6c39c123a
sktakyi <s.k.takyi5@gmail.com>
2024-06-20 05:07:30 +0000

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#assign variables
+principal = 1000
+rate = 0.05
+time = 3
+
+#computes varibales
+interest = principal * rate * time
+
+#display computed variable results 
+print (f"The simple interest is {interest}")
