5fdccb642ed5f01aa723296d74b9c989dbedebe3
sktakyi <s.k.takyi5@gmail.com>
2024-06-26 19:38:38 +0000

    Weather Recommendation Program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,10 +3,10 @@ weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
 #display results
 if weather == "sunny":
-    print ("Wear a t-shirt and sunglasses.")
+    print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
-    print ("Don’t forget your umbrella and a raincoat.")
+    print("Don’t forget your umbrella and a raincoat.")
 elif weather == "cold":
-    print ("Make sure to wear a warm coat and a scarf.")
+    print("Make sure to wear a warm coat and a scarf.")
 else:
-    print ("invalid input response")
+    print("invalid input response")
