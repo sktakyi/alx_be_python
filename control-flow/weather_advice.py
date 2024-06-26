efa1ce6cca75b6964ab855305d52a7b22b07f406
sktakyi <s.k.takyi5@gmail.com>
2024-06-26 19:40:12 +0000

    Weather Recommendation Program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -9,4 +9,4 @@ elif weather == "rainy":
 elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
-    print("invalid input response")
+    print("Sorry, I don't have recommendations for this weather.")
