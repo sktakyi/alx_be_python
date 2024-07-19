b184298cfa652c1801c612cc22efd2cb0c475541
sktakyi <s.k.takyi5@gmail.com>
2024-07-19 19:00:37 +0000

    Initial-Commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -17,9 +17,10 @@ class PrintBook(Book):
 
 class Library:
     def __init__(self):
-        self.books = []
+        self.books = [] 
 
     def add_book(self, book):
         self.books.append(book)
+        
     def list_books(self, book):
          self.books.sort(book)
