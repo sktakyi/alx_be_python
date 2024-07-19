ac5bbfffdb6d1932f64c9054c6874d0dc861ee31
sktakyi <s.k.takyi5@gmail.com>
2024-07-19 18:09:33 +0000

    Initial-Commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,12 +2,16 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+
 class EBook(Book):
-    def __str__(self, file_size):
-        self.file_size = file_size
+    def __init__(self):
+        def __str__(self, file_size):
+            self.file_size = file_size
+
 class PrintBook(Book):
-    def __str__(self, page_count):
-        self.page_count = page_count
+    def __init__(self):
+        def __str__(self, page_count):
+            self.page_count = page_count
 
 class Library:
     def __init__(self):
