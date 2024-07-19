7ddc6205cd936e09c73932c36c92fd3710a64819
sktakyi <s.k.takyi5@gmail.com>
2024-07-19 21:02:51 +0000

    Initial-Commit

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,23 +1,26 @@
 class Book:
     def __init__(self, title, author):
         self.title = title
-        self.author = author
+        self.author = author  
 
+    def __str__(self):
+         return f"{self.title} by {self.author}"
+    
 class EBook(Book):
-        def __init__(self, title, author, file_size):
-            super().__init__(title, author)
-            self.file_size = file_size
-            return
-        def __str__(self):
-             return
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size  
+
+    def __str__(self):
+        return f"{super().__str__()} [E-Book, File Size: {self.file_size}MB]"
 
 class PrintBook(Book):
-        def __init__(self, title, author, page_count):
-             super().__init__(title, author)
-             self.page_count = page_count
-             return
-        def __str__(self):
-             return
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+         return f"{super().__str__()} [Print Book, Page Count: {self.page_count}]"
 
 class Library:
     def __init__(self):
